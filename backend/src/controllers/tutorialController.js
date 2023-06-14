727e1d30dd4fdd8333d7354c6b5f4084692f9f05
Azkunaga <aritzazkunaga@users.noreply.github.com>
2023-06-14 20:06:54 +0300

    tutorial controller and service

---
+++ backend/src/controllers/tutorialController.js
@@ -0,0 +1,21 @@
+require('dotenv').config({path: "env/local.env"})
+
+const express = require('express');
+
+const getPart = () => {
+
+}
+
+const partPuntuatu = () => {
+    
+}
+
+const postPart = () => {
+
+}
+
+module.exports = {
+    getPart,
+    partPuntuatu,
+    postPart,
+}
