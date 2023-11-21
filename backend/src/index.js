66a436a05e959920fcd15abdd340e15a55b7fc34
Azkunaga <aritzazkunaga@users.noreply.github.com>
2023-11-21 12:02:11 +0200

    dockerizing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const server = require("./server");
 const mongodbConnection = require('./config/mongodb');
 
-const port = process.env.DEV_PORT || 3000;
+const port = process.env.DEV_PORT || 3001;
 
 const StartServer = () => {
     server.listen(port, () =>{
